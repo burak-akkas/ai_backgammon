bb92a50453abbc6ec766b9eb1694d51ae6633f3a
javasch <burak.akkas@outlook.com>
2016-06-07 06:19:33 +0300

    code restructured, cleaned

--- core/src/com/cse/bgai/Board.java
+++ core/src/com/cse/bgai/Board.java
@@ -15,11 +15,10 @@ public class Board {
     // logic
     private int logicBoard[];
 
-    public int capturedWhitePieces = 0;
-    public int capturedBlackPieces = 0;
-
-    private int bearoffWhitePieces = 0;
-    private int bearoffBlackPieces = 0;
+    public final int WHITE_CAPTURED_FIELD = 24;
+    public final int BLACK_CAPTURED_FIELD = 25;
+    public final int WHITE_BEAROFF_FIELD = 26;
+    public final int BLACK_BEAROFF_FIELD = 27;
 
     public ArrayList possibleMoves;
 
@@ -39,8 +38,8 @@ public class Board {
 
     // constructor
     public Board() {
-        logicBoard = new int[24];
-        graphicBoard = new Sprite[24][15];
+        logicBoard = new int[28];
+        graphicBoard = new Sprite[28][15];
 
         possibleMoves = new ArrayList<Integer>();
 
@@ -56,7 +55,7 @@ public class Board {
 
     // initialize
     public void init() {
-        for(int i = 0; i < logicBoard.length; i++) { logicBoard[i] = 0; }
+        for(int i = 0; i < 28; i++) { logicBoard[i] = 0; }
 
         // white = 1
         // black = -1
@@ -97,7 +96,7 @@ public class Board {
     public void placeCheckers() {
         int coordX = 0, coordY = 0;
 
-        for(int i = 0; i < 24; i++) {
+        for(int i = 0; i < 28; i++) {
             for(int k = 5; k < 15; k++) {
                 Sprite empty = new Sprite(emptyCheckerTexture, 0, 0, 200, 200);
                 empty.setBounds(coordX, coordY, 50, 50);
@@ -106,7 +105,7 @@ public class Board {
             }
         }
 
-        for(int i = 0; i < logicBoard.length; i++) {
+        for(int i = 0; i < 28; i++) {
 
             if(i >= 0 && i <= 5) {
                 coordX = (i * 50) + 32;
@@ -168,7 +167,7 @@ public class Board {
                 }
 
                 if (logicBoard[i] == 0) {  // if empty
-                   // TODO: be careful about here
+                    // TODO: be careful about here
                     //System.out.println("Empty, Pos X=" + coordX + " Pos Y="+ coordY);
                     Sprite empty = new Sprite(emptyCheckerTexture, 0, 0, 200, 200);
                     empty.setBounds(coordX, coordY, 50, 50);
@@ -178,161 +177,153 @@ public class Board {
             }
         }
 
+        // special cases
+        Sprite whiteCaptureSprite = new Sprite(whiteCheckerTexture, 0, 0, 200, 200);
+        whiteCaptureSprite.setSize(40, 40);
+        whiteCaptureSprite.setBounds(335, 330, 40, 40);
+        whiteCaptureSprite.setPosition(335, 330);
+        graphicBoard[24][0] = whiteCaptureSprite;
+
+        Sprite blackCaptureSprite = new Sprite(blackCheckerTexture, 0, 0, 200, 200);
+        blackCaptureSprite.setSize(40, 40);
+        blackCaptureSprite.setBounds(335, 255, 40, 40);
+        blackCaptureSprite.setPosition(335, 255);
+        graphicBoard[25][0] = blackCaptureSprite;
     }
 
-    public boolean move(int from, int to, int dice) {
-        int fromColor = getColor(logicBoard[from]);
-        int toColor = getColor(logicBoard[to]);
+    public boolean reenter(int from, int to, int dice) {
+        int fromColor = getColor(from);
+        int toColor = getColor(to);
 
-        if(fromColor == 1) { // white goes left 0 -> 23
-            // black goes right 23 -> 0
-            dice = -dice;
-        }
+        if(fromColor == 1) { dice = -dice; };
 
-        System.out.println("Dice: " + dice);
+        int fieldSelector = 0;
+        if(fromColor == 1) { fieldSelector = WHITE_CAPTURED_FIELD; }
+        else { fieldSelector = BLACK_CAPTURED_FIELD; }
 
-        if(fromColor > 0) {
-            if(capturedWhitePieces != 0) {
-                System.out.println("YOU CANT MOVE. YOU MUST RETURN YOUR CAPTURED PIECES FIRST.");
-                return false;
-            }
-        } else {
-            if(capturedBlackPieces != 0) {
-                System.out.println("YOU CANT MOVE. YOU MUST RETURN YOUR CAPTURED PIECES FIRST.");
-                return false;
-            }
-        }
-
-        System.out.println("FROM-TO= " + (from - to));
-        if(from - to == dice) {
-
-            if(canCapture(from, to, dice)) {
-                System.out.println("CAN CAPTURE!");
-
-                if(toColor > 0) { // white piece
-                    logicBoard[from]++;
-                    logicBoard[to] -= 2; // 1 for captured piece, 1 for new piece
-                    capturedWhitePieces++;
-
-                } else { // black piece
-                    logicBoard[from]--;
-                    logicBoard[to] += 2;
-                    capturedBlackPieces++;
-                }
-
-                System.out.println("Captured white Pieces: " + capturedWhitePieces);
-                System.out.println("Captured black Pieces: " + capturedBlackPieces);
-
-                return true;
-            }
-
-            if(canMove(from, to, dice)) {
-                System.out.println("CAN MOVE!");
-
-                if(fromColor > 0) { // white
-                    logicBoard[from]--;
-                    System.out.println("FROM: " + logicBoard[from]);
-                    logicBoard[to]++;
-                    System.out.println("TO: " + logicBoard[to]);
-                } else {
-                    logicBoard[from]++;
-                    logicBoard[to]--;
-                }
-
-                return true;
-            }
-
-            placeCheckers();
-        }
-
-        System.out.println("EROL?");
-
-        return false;
-    }
-
-    public boolean canCapture(int from, int to, int dice) { // kýrma
-        int fromColor = getColor(logicBoard[from]);
-        int toColor = getColor(logicBoard[to]);
-
-        // check capture movement
-        if(toColor != 0 && toColor != fromColor && Math.abs(logicBoard[to]) == 1) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public boolean canMove(int from, int to, int dice) { // hareket
-        int fromColor = getColor(logicBoard[from]);
-        int toColor = getColor(logicBoard[to]);
-
-        // check empty space and same color movement
-        if(toColor == 0 || toColor == fromColor) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public boolean canBearOff(int color) { // toplama
-
-        int counter = 0;
-
-        if(color > 0) {
-            if(capturedWhitePieces != 0) {
-                return false;
-            }
-
-            for(int i = 0; i < 6; i++) {
-                counter += logicBoard[i];
-            }
-        } else {
-            if(capturedBlackPieces != 0) {
-                return false;
-            }
-
-            for(int i = 18; i < 24; i++) {
-                counter += logicBoard[i];
-            }
-        }
-
-        if(Math.abs(counter) == 15) {
-            System.out.println("CAN BEAROFF!");
-            return true;
-        }
-
-        System.out.println("CANNOT BEAROFF");
-        return false;
-    }
-
-    public boolean bearOff(int from, int dice) {
-        int fromColor = getColor(logicBoard[from]);
-
-        if(fromColor == 1) { // white goes right 0 -> 23
-                             // black goes left 23 -> 0
-            dice = -dice;
-        }
-
-        if(canBearOff(fromColor)) {
-
-            if(fromColor == 1) {
-                logicBoard[from]--;
-                bearoffWhitePieces++;
-            } else {
-                logicBoard[from]++;
-                bearoffBlackPieces++;
-            }
-
-            return true;
+        if(logicBoard[fieldSelector] != 0) {
+            reenterMove(fromColor, dice);
         } else {
             return false;
         }
+
+        return true;
+    }
+
+    public boolean move(int from, int to, int dice) {
+        int fromColor = getColor(from);
+        int toColor = getColor(to);
+
+        if(fromColor == 1) { dice = -dice; };
+
+        int fieldSelector = 0;
+        if(fromColor == 1) { fieldSelector = WHITE_CAPTURED_FIELD; }
+        else { fieldSelector = BLACK_CAPTURED_FIELD; }
+
+        if(from - to != dice) { return false; }
+
+        // if selected color not have a captured piece
+        if(logicBoard[fieldSelector] == 0) {
+            // its a normal move
+            if(fromColor == toColor || toColor == 0) {
+                normalMove(fromColor, from, to);
+            }
+            // its a capture move
+            else if(fromColor != toColor && Math.abs(logicBoard[to]) == 1) {
+                captureMove(fromColor, from, to);
+            }
+            // its a bearoff move
+            else if(to == fieldSelector && canBearoff(fromColor)) {
+                bearoffMove(fromColor, from);
+            }
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void normalMove(int color, int from, int to) {
+        if(color == 1) { // white
+            logicBoard[from]--;
+            logicBoard[to]++;
+        } else {        // black
+            logicBoard[from]++;
+            logicBoard[to]--;
+        }
+    }
+
+    public void captureMove(int color, int from, int to) {
+        if(color == 1) {
+            logicBoard[from]--;
+            logicBoard[to] += 2;
+            logicBoard[BLACK_CAPTURED_FIELD]++;
+        } else {
+            logicBoard[from]++;
+            logicBoard[to] -= 2;
+            logicBoard[WHITE_CAPTURED_FIELD]++;
+        }
+    }
+
+    public void bearoffMove(int color, int from) {
+        if(color == 1) { // white
+            logicBoard[WHITE_BEAROFF_FIELD]++;
+            logicBoard[from]--;
+        } else {        // black
+            logicBoard[BLACK_BEAROFF_FIELD]++;
+            logicBoard[from]++;
+        }
+    }
+
+    // changed: to -> dice
+    public void reenterMove(int color, int dice) {
+        if(color == 1) {
+            logicBoard[WHITE_CAPTURED_FIELD]--;
+
+            if(logicBoard[-dice - 1] == -1) {
+                logicBoard[BLACK_CAPTURED_FIELD]++;
+                logicBoard[-dice - 1]++;
+            }
+
+            logicBoard[-dice - 1]++;
+        } else {
+            logicBoard[BLACK_CAPTURED_FIELD]--;
+
+            if(logicBoard[24 - dice] == 1) {
+                logicBoard[WHITE_CAPTURED_FIELD]++;
+                logicBoard[24 - dice]--;
+            }
+
+            logicBoard[24 - dice]--;
+        }
+    }
+
+    public boolean canBearoff(int color) {
+
+        int counter = 0;
+
+        if(color == 1) {
+            for(int i = 18; i < 24; i++) {
+                if(getColor(i) == color) {
+                    counter += logicBoard[i];
+                }
+            }
+        } else {
+            for(int i = 0; i < 6; i++) {
+                if(getColor(i) == color) {
+                    counter += logicBoard[i];
+                }
+            }
+        }
+
+        if(Math.abs(counter) == 15) { return true; }
+        return false;
     }
 
     public int checkWinner() {
-        if(bearoffWhitePieces == 15) {
+        if(logicBoard[WHITE_BEAROFF_FIELD] == 15) {
             return 1;
-        } else if(bearoffBlackPieces == 15) {
+        } else if(logicBoard[BLACK_BEAROFF_FIELD] == -15) {
             return -1;
         } else {
             return 0;
@@ -354,7 +345,10 @@ public class Board {
 
     }
 
-    public int getColor(int checker) {
+    public int getColor(int index) {
+
+        int checker = logicBoard[index];
+
         if(checker > 0) {
             return 1;
         } else if(checker < 0) {
@@ -371,25 +365,36 @@ public class Board {
 
         System.out.println("Find piece index with click X: " + x + ", Y: " + y);
 
-        for (int i = 0; i < 24; i++) {
+        for (int i = 0; i < 28; i++) {
             for(int j = 0; j < 15; j++) {
 
                 //if(graphicBoard[i][j] != null) {
-                    if (x > graphicBoard[i][j].getX() && x < graphicBoard[i][j].getX() + 50) {
-                        if (y > graphicBoard[i][j].getY() && y < graphicBoard[i][j].getY() + 50) {
+                if (x > graphicBoard[i][j].getX() && x < graphicBoard[i][j].getX() + 50) {
+                    if (y > graphicBoard[i][j].getY() && y < graphicBoard[i][j].getY() + 50) {
 
-                            index = i;
+                        index = i;
 
-                            graphX = (int)graphicBoard[i][j].getX();
-                            graphY = (int)graphicBoard[i][j].getY();
+                        graphX = (int)graphicBoard[i][j].getX();
+                        graphY = (int)graphicBoard[i][j].getY();
 
-                            System.out.println("Found, index: " + i);
-                            break;
-                        }
+                        System.out.println("Found, index: " + i);
+                        break;
                     }
-               // }
+                }
+                // }
             }
         }
+
+        /*
+        // if index equals to zero,
+        // check for special sprite indexes
+        // whiteCaptureSprite.setBounds(335, 330, 40, 40);
+        if(index == 0) {
+            if(x > 335 && x < 375) {
+                if(y > )
+            }
+        }
+        */
 
         int values[] = new int[3];
         values[0] = index; // index found
@@ -403,7 +408,7 @@ public class Board {
     public void draw(SpriteBatch batch) {
         boardSprite.draw(batch);
 
-        for(int i = 0; i < 24; i++) {
+        for(int i = 0; i < 28; i++) {
             for(int j = 0; j < 15; j++) {
                 /*if(graphicBoard[i][j] != null)*/ graphicBoard[i][j].draw(batch);
             }
--- core/src/com/cse/bgai/bgAI.java
+++ core/src/com/cse/bgai/bgAI.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
@@ -20,7 +21,6 @@ import java.util.ArrayList;
 public class bgAI extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	SpriteBatch batch;
-	Texture img;
 	Board board;
 	private Rectangle rect;
 
@@ -44,7 +44,6 @@ public class bgAI extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		//img = new Texture(Gdx.files.local("core/assets/badlogic.jpg"));
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false);
 
@@ -75,28 +74,28 @@ public class bgAI extends ApplicationAdapter {
 					}
 
 					if(!toSelected && isDiceRolled && toIndex == 0) {
-							// CHANGED: 160
-							rect.setPosition(Gdx.input.getX(), Gdx.graphics.getWidth() - 132 - Gdx.input.getY());
-							camera.unproject(new Vector3(rect.getX(), rect.getY(), 0));
+						// CHANGED: 160
+						rect.setPosition(Gdx.input.getX(), Gdx.graphics.getWidth() - 132 - Gdx.input.getY());
+						camera.unproject(new Vector3(rect.getX(), rect.getY(), 0));
 
-							//board.move(5, 2, 3);
-							//board.move(11, 13, 2);
-							int values[] = board.convertClickToIndex((int)rect.getX(), (int)rect.getY());
+						//board.move(5, 2, 3);
+						//board.move(11, 13, 2);
+						int values[] = board.convertClickToIndex((int)rect.getX(), (int)rect.getY());
 
-							selector.setX(values[1]);
-							selector.setY(values[2]);
+						selector.setX(values[1]);
+						selector.setY(values[2]);
 
-							fromIndex = values[0];
+						fromIndex = values[0];
 
-							fromSelected = true;
-							//board.placeCheckers();
+						fromSelected = true;
+						//board.placeCheckers();
 					}
 				}
 
 				if(button == Input.Buttons.RIGHT) {
 
 					if(fromSelected && !toSelected && isDiceRolled && toIndex == 0 ) {
-						rect.setPosition(Gdx.input.getX(), Gdx.graphics.getWidth() - 160 - Gdx.input.getY());
+						rect.setPosition(Gdx.input.getX(), Gdx.graphics.getWidth() - 132 - Gdx.input.getY());
 						camera.unproject(new Vector3(rect.getX(), rect.getY(), 0));
 
 						int values[] = board.convertClickToIndex((int)rect.getX(), (int)rect.getY());
@@ -118,129 +117,99 @@ public class bgAI extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		if(board.checkWinner() == 0) {
-			if(fromSelected && toSelected) {
-				// means MOVE!
-				if(board.getLogicBoard()[fromIndex] > 0 && isWhite) { // white playing
-					if(dices.contains(Math.abs(fromIndex - toIndex))) {
-						int current = dices.indexOf(Math.abs(fromIndex - toIndex));
-
-						System.out.println("Current dice index: " + current + ", value: " + dices.get(current));
-						/*
-						if(board.capturedWhitePieces > 0) {
-							if(board.move(board.capturedWhitePieces, toIndex, dices.get(current))) {
-
-								currentlyPlayedDice = dices.get(current);
-
-								dices.remove(current);
-							}
-						}*/
-						if(board.canBearOff(1)) {
-							if(board.bearOff(fromIndex, dices.get(current))) {
-
-								currentlyPlayedDice = dices.get(current);
-
-								dices.remove(current);
-							}
-						} else {
-							if(board.move(fromIndex, toIndex, dices.get(current))) {
-
-								currentlyPlayedDice = dices.get(current);
-
-								dices.remove(current);
-							}
-						}
-					} else {
-						System.out.println("YOU CANT PLAY LIKE THAT!");
-					}
-
-					if(dices.size() == 0) {
-						isWhite = false;
-						isDiceRolled = false;
-						currentlyPlayedDice = 0;
-					}
-
-				} else if(board.getLogicBoard()[fromIndex] < 0 && !isWhite) { // black playing
-					if(dices.contains(Math.abs(fromIndex - toIndex))) {
-						int current = dices.indexOf(Math.abs(fromIndex - toIndex));
-
-						/*
-						if(board.capturedBlackPieces > 0) {
-							if(board.move(-board.capturedBlackPieces, toIndex, dices.get(current))) {
-
-								currentlyPlayedDice = dices.get(current);
-
-								dices.remove(current);
-							}
-						}
-						*/
-						if(board.canBearOff(-1)) {
-							if(board.bearOff(fromIndex, dices.get(current))) {
-
-								currentlyPlayedDice = dices.get(current);
-
-								dices.remove(current);
-							}
-						} else {
-							if(board.move(fromIndex, toIndex, dices.get(current))) {
-
-								currentlyPlayedDice = dices.get(current);
-
-								dices.remove(current);
-							}
-						}
-
-					} else {
-						System.out.println("YOU CANT PLAY LIKE THAT!");
-					}
-
-					if(dices.size() == 0) {
-						isWhite = true;
-						isDiceRolled = false;
-						currentlyPlayedDice = 0;
-					}
-				} else {
-					System.out.println("NOT YOUR TURN!");
-				}
-
-				//board.move(fromIndex, toIndex, Math.abs(fromIndex - toIndex));
-
-				System.out.println("MOVE! FROM = " + fromIndex + "TO = " + toIndex);
-
-				toSelected = false;
-				fromSelected = false;
-				fromIndex = 0;
-				toIndex = 0;
-
-				selector.setX(0);
-				selector.setY(0);
-
-				board.placeCheckers();
-			}
-		}
+		update();
 
 		batch.begin();
-		board.draw(batch);
-		dice.getFirstSprite().draw(batch);
-		dice.getSecondSprite().draw(batch);
-
-		// text
-		font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
-		font.draw(batch, isWhite ? "WHITE'S TURN" : "BLACK'S TURN", 270, 635);
-		if(!isDiceRolled) { font.draw(batch, "(ROLL THE DICE!)", 410, 635); }
-		if(isDiceRolled) { font.draw(batch, "(LAST PLAYED: " + (currentlyPlayedDice == 0 ? "NOTHING" : currentlyPlayedDice) + ")", 410, 635); }
-		font.draw(batch, "WHITE", 330, 450);
-		font.draw(batch, "C: " + board.capturedWhitePieces, 340, 420);
-		font.draw(batch, "BLACK ", 330, 250);
-		font.draw(batch, "C: " + board.capturedBlackPieces, 340, 220);
-
+			board.draw(batch);
+			dice.getFirstSprite().draw(batch);
+			dice.getSecondSprite().draw(batch);
+			drawText();
 		batch.end();
 
 		if(selector.getY() != 0 && selector.getX() != 0) {
 			sr.begin(ShapeRenderer.ShapeType.Line);
 			sr.setColor(Color.RED);
+			if(fromIndex == 24 || fromIndex == 25) { selector.setHeight(40); selector.setWidth(40); }
+			else { selector.setHeight(50); selector.setWidth(50); }
 			sr.rect(selector.getX(), selector.getY(), selector.getWidth(), selector.getHeight());
 			sr.end();
 		}
+	}
+
+	public void update() {
+		if(board.checkWinner() == 0) {
+			if(fromSelected && toSelected) {
+				if(dices.contains(Math.abs(fromIndex - toIndex))) {
+					int current = dices.indexOf(Math.abs(fromIndex - toIndex));
+					int currentDice = dices.get(current);
+
+					if((board.getLogicBoard()[fromIndex] < 0 && !isWhite && board.move(fromIndex, toIndex, currentDice)
+							|| (board.getLogicBoard()[fromIndex] > 0 && isWhite && board.move(fromIndex, toIndex, currentDice)))) {
+
+						currentlyPlayedDice = dices.get(current);
+
+						dices.remove(current);
+					} else {
+						System.out.println("NOT YOUR TURN.");
+					}
+				} else if((dices.contains(Math.abs(toIndex + 1)) && isWhite)
+						|| (dices.contains(Math.abs(24 - (toIndex + 1))) && !isWhite)) {
+
+					int current = 0;
+
+					if(isWhite) { current = dices.indexOf(Math.abs(toIndex + 1)); }
+					else { current = dices.indexOf(Math.abs(24 - (toIndex + 1))); }
+
+					int currentDice = dices.get(current);
+
+					if((board.getLogicBoard()[fromIndex] < 0 && !isWhite && board.reenter(fromIndex, toIndex, currentDice)
+							|| (board.getLogicBoard()[fromIndex] > 0 && isWhite && board.reenter(fromIndex, toIndex, currentDice)))) {
+
+						currentlyPlayedDice = dices.get(current);
+
+						dices.remove(current);
+					} else {
+						System.out.println("NOT YOUR TURN.");
+					}
+
+				} else {
+					System.out.println("YOU CANT PLAY LIKE THAT!");
+				}
+
+				if(dices.size() == 0) {
+					isWhite = !isWhite;
+					isDiceRolled = false;
+					currentlyPlayedDice = 0;
+				}
+
+				reset();
+				board.placeCheckers();
+			}
+		}
+	}
+
+	public void drawText() {
+		// text
+		font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+		font.draw(batch, isWhite ? "WHITE'S TURN" : "BLACK'S TURN", 270, 635);
+		if(!isDiceRolled) { font.draw(batch, "(ROLL THE DICE!)", 410, 635); }
+		if(isDiceRolled) { font.draw(batch, "(LAST PLAYED: " + (currentlyPlayedDice == 0 ? "NOTHING" : currentlyPlayedDice) + ")", 410, 635); }
+		font.setColor(0.0f, 0.0f, 0.0f, 1.0f);
+		//font.draw(batch, "WHITE", 330, 450);
+		font.draw(batch, ""+board.getLogicBoard()[board.WHITE_CAPTURED_FIELD], 351, 355);
+		font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+		//font.draw(batch, "BLACK ", 330, 250);
+		font.draw(batch, "" + board.getLogicBoard()[board.BLACK_CAPTURED_FIELD], 351, 280);
+
+	}
+
+	public void reset() {
+		toSelected = false;
+		fromSelected = false;
+		fromIndex = 0;
+		toIndex = 0;
+
+		selector.setX(0);
+		selector.setY(0);
 	}
 }
